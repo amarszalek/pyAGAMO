9642eccf286547243f6b2075ebcb5fa840703a4e
amarszalek <amarszalek@pk.edu.pl>
2021-11-20 23:17:09 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy", "pika"]
+
+setup(
+    name="pygamoo",
+    version="0.0.1",
+    author="Adam Marszałek & Paweł Jarosz",
+    author_email="amarszalek@pk.edu.pl",
+    description="GAme theory based framework for MultiObjective Optimization in Python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amarszalek/pyGAMOO",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: Apache License 2.0",
+    ],
+)
