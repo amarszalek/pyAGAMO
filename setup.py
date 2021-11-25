79a7871f1b89d058e20d8be5158986fe73b07b6c
amarszalek <amarszalek@pk.edu.pl>
2021-11-25 22:13:25 +0100

    setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy", "pika"]
 
 setup(
     name="pygamootest",
-    version="0.0.7",
+    version="0.0.8",
     author="Adam Marszałek & Paweł Jarosz",
     author_email="amarszalek@pk.edu.pl",
     description="GAme theory based framework for MultiObjective Optimization in Python",
